d85b3d12a718ead3183a4ac26f3f1810482e0fdc
Aalivexy <aalivexy@outlook.com>
2026-01-31 15:53:10 +0800

    make authenticate_with_biometrics more elegant

--- src/bio.rs
+++ src/bio.rs
@@ -1,43 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2025 Aalivexy
- 
-use std::{
-    thread::{sleep, spawn},
-    time::Duration,
-};
+
 use windows::{
     Security::Credentials::UI::{
         UserConsentVerificationResult, UserConsentVerifier, UserConsentVerifierAvailability,
     },
     Win32::{
-        System::{
-            Threading::{AttachThreadInput, GetCurrentThreadId},
-            WinRT::IUserConsentVerifierInterop,
-        },
-        UI::{
-            Input::KeyboardAndMouse::SetFocus,
-            WindowsAndMessaging::{
-                BringWindowToTop, FindWindowW, GetForegroundWindow, GetWindowThreadProcessId,
-                HWND_DESKTOP, SetForegroundWindow,
-            },
-        },
+        System::WinRT::IUserConsentVerifierInterop,
+        UI::WindowsAndMessaging::{GA_ROOT, GetAncestor, GetForegroundWindow},
     },
-    core::{HSTRING, factory, w},
+    core::{HSTRING, factory},
 };
 use windows_future::IAsyncOperation;
 
 pub fn authenticate_with_biometrics() -> bool {
-    spawn(|| {
-        for _ in 0..40 {
-            sleep(Duration::from_millis(50));
-            center_security_prompt();
-        }
-    });
     unsafe {
+        let fg_hwnd = GetForegroundWindow();
+        let owner_hwnd = GetAncestor(fg_hwnd, GA_ROOT);
         factory::<UserConsentVerifier, IUserConsentVerifierInterop>()
             .unwrap()
             .RequestVerificationForWindowAsync::<IAsyncOperation<UserConsentVerificationResult>>(
-                HWND_DESKTOP,
+                owner_hwnd,
                 &HSTRING::new(),
             )
             .is_ok_and(|async_op| async_op.get() == Ok(UserConsentVerificationResult::Verified))
@@ -58,20 +41,4 @@ pub fn get_biometrics_status() -> i32 {
             }
         })
     })
-}
-
-fn center_security_prompt() {
-    let hwnd = unsafe { FindWindowW(w!("Credential Dialog Xaml Host"), None) };
-    if let Ok(hwnd) = hwnd {
-        unsafe {
-            let fg_hwnd = GetForegroundWindow();
-            let cur_id = GetCurrentThreadId();
-            let fg_id = GetWindowThreadProcessId(fg_hwnd, None);
-            let _ = AttachThreadInput(cur_id, fg_id, true);
-            let _ = SetForegroundWindow(hwnd);
-            let _ = BringWindowToTop(hwnd);
-            let _ = SetFocus(Some(hwnd));
-            let _ = AttachThreadInput(cur_id, fg_id, false);
-        }
-    }
 }
